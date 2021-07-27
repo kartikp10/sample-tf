b6f650e164815b720b2b63fe9ac642ed85a7887e
kartikp10 <kpande479@gmail.com>
2021-07-27 08:32:04 -0400

    Fix bucket

--- s3.tf
+++ s3.tf
@@ -44,16 +44,5 @@ resource "aws_s3_bucket" "data_science" {
 resource "aws_s3_bucket" "logs" {
   bucket = "${local.resource_prefix.value}-logs"
   acl    = "log-delivery-write"
-  versioning {
-    enabled = true
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = "aws:kms"
-        kms_master_key_id = "${aws_kms_key.logs_key.arn}"
-      }
-    }
-  }
   force_destroy = true
 }
