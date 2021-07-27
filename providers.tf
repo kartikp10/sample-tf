cd44e3c24eefdcd8d9db21bd279ce3e5e6e617f4
kartikp10 <kpande479@gmail.com>
2021-07-27 08:41:33 -0400

    Fix provider

--- providers.tf
+++ providers.tf
@@ -1,6 +1,4 @@
 provider "aws" {
-  alias      = "plain_text_access_keys_provider"
   region     = "us-west-1"
-  access_key = "AKIAIOSFODNN7EXAMPLE"
-  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
 }
