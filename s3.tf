1aa7f832a329abea1e87df51f319767644275a6d
Kartikeya Pande <kpande479@gmail.com>
2021-07-30 10:40:14 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -41,8 +41,3 @@ resource "aws_s3_bucket" "data_science" {
   }
 }
 
-resource "aws_s3_bucket" "logs" {
-  bucket = "${local.resource_prefix.value}-logs"
-  acl    = "log-delivery-write"
-  force_destroy = true
-}
