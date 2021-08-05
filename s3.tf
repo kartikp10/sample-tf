be59103f157ca80e87405d7539575915a8e4915e
Kartikeya Pande <kpande479@gmail.com>
2021-08-05 13:25:58 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,9 @@
-  1 module "s3_bucket" {
-  2   # source = "git::ssh://git@github.com/cloudposse/terraform-aws-lb-s3-bucket.gi    t"
-  3   #source = "git::https://github.com/cloudposse/terraform-aws-lb-s3-bucket.git"
-  4   source = "cloudposse/lb-s3-bucket/aws"
-      # Cloud Posse recommends pinning every module to a specific version
-  5   # version     = "x.x.x"
-  6   namespace = "eg"
-  7   stage     = "test"
-  8   name      = "bucket"
-  9 }
+module "s3_bucket" {
+  # source = "git::ssh://git@github.com/cloudposse/terraform-aws-lb-s3-bucket.git"
+  # source = "git::https://github.com/cloudposse/terraform-aws-lb-s3-bucket.git"
+  # source = "git@github.com:cloudposse/terraform-aws-lb-s3-bucket.git"
+  source = "cloudposse/lb-s3-bucket/aws"
+  namespace = "eg"
+  stage     = "test"
+  name      = "bucket"
+}
