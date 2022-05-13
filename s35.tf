c7b437467a9ce5d02b0fba53bc2b5500f9cae537
kartikp10 <kpande479@gmail.com>
2022-05-13 11:40:36 -0400

    config

---
+++ s35.tf
@@ -0,0 +1,54 @@
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "financials" {
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-financials"
+  acl           = "private"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "operations" {
+  # bucket is not encrypted
+  # bucket does not have access logs
+  bucket = "${local.resource_prefix.value}-operations"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "data_science" {
+  # bucket is not encrypted
+  bucket = "${local.resource_prefix.value}-data-science"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  logging {
+    target_bucket = aws_s3_bucket.logs.id
+    target_prefix = "log/"
+  }
+}
+
+resource "aws_s3_bucket" "logs" {
+  bucket        = "${local.resource_prefix.value}-logs"
+  acl           = "log-delivery-write"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "logs22" {
+  bucket        = "${local.resource_prefix.value}-logs"
+  acl           = "log-delivery-write"
+  force_destroy = true
+}
