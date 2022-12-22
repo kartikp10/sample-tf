b8d6f6709d9ec0aa17663af21600752cccfc5cc2
kaypee10 <94990074+kaypee10@users.noreply.github.com>
2022-12-22 14:40:04 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -40,9 +40,3 @@ resource "aws_s3_bucket" "data_science" {
     target_prefix = "log/"
   }
 }
-
-resource "aws_s3_bucket" "logs" {
-  bucket = "${local.resource_prefix.value}-logs"
-  acl    = "log-delivery-write"
-  force_destroy = false
-}
