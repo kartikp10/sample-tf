5d81914f235ad23ca0a31824d731fbd54f8dc283
Kartikeya Pande <kpande479@gmail.com>
2023-03-27 15:52:17 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -33,7 +33,7 @@ resource "aws_s3_bucket" "data_science" {
   bucket = "${local.resource_prefix.value}-data-science"
   acl    = "private"
   versioning {
-    enabled = true
+    enabled = false
   }
   logging {
     target_bucket = "${aws_s3_bucket.logs.id}"
